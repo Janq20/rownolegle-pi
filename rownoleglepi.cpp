dd3e1ff2f6605ab4712de0d5d264838e5656275d
Janq20 <janoxgra@gmail.com>
2025-12-31 14:50:41 +0100

    Implementacja sekwencyjnego algorytmu obliczania PI metoda prostokatow

--- rownoleglepi.cpp
+++ rownoleglepi.cpp
@@ -8,7 +8,16 @@ using namespace std;
 int main(int argc, char* argv[]) {
     long long num_steps = 100000000;
     int num_threads = 4;
+    double step = 1.0 / (double)num_steps;
+    double sum = 0.0;
 
+    for (long long i = 0; i < num_steps; i++) {
+        double x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    double pi = sum * step;
+    cout << "Wynik PI: " << pi << endl;
     if (argc == 3) {
         num_steps = stoll(argv[1]);
         num_threads = stoi(argv[2]);
