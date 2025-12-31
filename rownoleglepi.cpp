d7ffad3b6251ffa83a2e2677b38a61397401f93a
Janq20 <janoxgra@gmail.com>
2025-12-31 15:00:14 +0100

    Implementacja wyrażenia lambda dla zadania obliczeniowego wątku

--- rownoleglepi.cpp
+++ rownoleglepi.cpp
@@ -20,6 +20,14 @@ int main(int argc, char* argv[]) {
     double step = 1.0 / (double)num_steps;
     double sum = 0.0;
     auto start = high_resolution_clock::now();
+    auto worker = [step, num_steps, num_threads, &partial_sums](int id) {
+        double local_sum = 0.0;
+        for (long long i = id; i < num_steps; i += num_threads) {
+            double x = (i + 0.5) * step;
+            local_sum += 4.0 / (1.0 + x * x);
+        }
+        partial_sums[id] = local_sum;
+        };
     for (long long i = 0; i < num_steps; i++) {
         double x = (i + 0.5) * step;
         sum += 4.0 / (1.0 + x * x);
