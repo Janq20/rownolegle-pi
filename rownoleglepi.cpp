e1b5428ec696bfc96d49a723c4eb890b31faacfe
Janq20 <janoxgra@gmail.com>
2025-12-31 14:49:43 +0100

    Dodanie obslugi argumentow linii komend i konfiguracji poczatkowej

--- rownoleglepi.cpp
+++ rownoleglepi.cpp
@@ -1,9 +1,19 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "Projekt: Obliczanie PI metoda calkowania numerycznego" << endl;
-    cout << "Szkielet programu przygotowany." << endl;
+    long long num_steps = 100000000;
+    int num_threads = 4;
+
+    if (argc == 3) {
+        num_steps = stoll(argv[1]);
+        num_threads = stoi(argv[2]);
+    }
+
+    cout << "Konfiguracja: " << num_steps << " krokow, " << num_threads << " watkow." << endl;
     return 0;
 }
