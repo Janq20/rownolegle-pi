c19be38f19f3115f25c11b882d035783b3f59909
Janq20 <janoxgra@gmail.com>
2025-12-31 14:47:51 +0100

    Commit 1: Inicjalizacja projektu, szkielet main.cpp i benchmark.py

---
+++ benchmark.py
@@ -0,0 +1,7 @@
+import os
+
+def main():
+    print("Szkielet skryptu do testow.")
+
+if __name__ == "__main__":
+    main()
---
+++ rownoleglepi.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Projekt: Obliczanie PI metoda calkowania numerycznego" << endl;
+    cout << "Szkielet programu przygotowany." << endl;
+    return 0;
+}
