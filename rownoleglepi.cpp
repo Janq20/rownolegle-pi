dfc7a5e1377e89a3c1672d66cd22b4a7892e080f
Janq20 <janoxgra@gmail.com>
2026-01-05 11:17:01 +0100

    Implementacja uruchamiania wątków i synchronizacji join

--- rownoleglepi.cpp
+++ rownoleglepi.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 
 using namespace std;
-using namespace std::chrono; // Dodano, aby high_resolution_clock był widoczny
+using namespace std::chrono;
 
 int main(int argc, char* argv[]) {
     long long num_steps = 100000000;
@@ -15,11 +15,14 @@ int main(int argc, char* argv[]) {
         num_steps = stoll(argv[1]);
         num_threads = stoi(argv[2]);
     }
+
     vector<double> partial_sums(num_threads, 0.0);
     vector<thread> threads;
     double step = 1.0 / (double)num_steps;
     double sum = 0.0;
+
     auto start = high_resolution_clock::now();
+
     auto worker = [step, num_steps, num_threads, &partial_sums](int id) {
         double local_sum = 0.0;
         for (long long i = id; i < num_steps; i += num_threads) {
@@ -28,13 +31,20 @@ int main(int argc, char* argv[]) {
         }
         partial_sums[id] = local_sum;
         };
-    double pi = sum * step;
-    cout << "Wynik PI: " << pi << endl;
-    if (argc == 3) {
-        num_steps = stoll(argv[1]);
-        num_threads = stoi(argv[2]);
+
+    for (int i = 0; i < num_threads; ++i) {
+        threads.emplace_back(worker, i);
     }
 
+    for (auto& th : threads) {
+        th.join();
+    }
+
+    double pi = sum * step;
+
+    cout << "Wynik PI: " << pi << endl;
+
     cout << "Konfiguracja: " << num_steps << " krokow, " << num_threads << " watkow." << endl;
+
     return 0;
 }
